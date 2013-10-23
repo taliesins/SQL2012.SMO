94da1d990720551fd62c00c06cddd7239323dee5
Taliesin Sisson <taliesins@yahoo.com>
2013-10-23 10:31:34 +0100

    Initial check in

---
+++ tools/chocolateyInstall.ps1
@@ -0,0 +1,28 @@
+$package = 'SQL20012.SMO'
+
+try {
+  $params = @{
+    packageName = $package;
+    fileType = 'msi';
+    silentArgs = '/quiet';
+    url = 'http://download.microsoft.com/download/4/B/1/4B1E9B0E-A4F3-4715-B417-31C82302A70A/ENU/x86/SharedManagementObjects.msi';
+    url64bit = 'http://download.microsoft.com/download/4/B/1/4B1E9B0E-A4F3-4715-B417-31C82302A70A/ENU/x64/SharedManagementObjects.msi';
+  }
+
+  Install-ChocolateyPackage @params
+
+  # install both x86 and x64 editions of SMO since x64 supports both
+  # to install both variants of powershell, both variants of SMO must be present
+  $IsSytem32Bit = (($Env:PROCESSOR_ARCHITECTURE -eq 'x86') -and `
+    ($Env:PROCESSOR_ARCHITEW6432 -eq $null))
+  if (!$IsSytem32Bit)
+  {
+    $params.url64bit = $params.url
+    Install-ChocolateyPackage @params
+  }
+
+  Write-ChocolateySuccess $package
+} catch {
+  Write-ChocolateyFailure $package "$($_.Exception.Message)"
+  throw
+}
