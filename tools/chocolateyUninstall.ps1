e82c58c4244aaa771fbe996d524ffcfb717c3c20
Rory Plaire <codekaizen@gmail.com>
2014-09-30 13:36:59 -0700

    Fixing x86 MSI uninstall
    Added x86 msi product id and changed 64-bit OS detection so both
    packages are uninstalled correctly.

--- tools/chocolateyUninstall.ps1
+++ tools/chocolateyUninstall.ps1
@@ -1,12 +1,12 @@
 # HKEY_LOCAL_MACHINE\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\
-$msiId = ''
+$msiId = '{DA1C1761-5F4F-4332-AB9D-29EDF3F8EA0A}'
 $msiId64 = '{FA0A244E-F3C2-4589-B42A-3D522DE79A42}'
 
 $package = 'SQL2012.SMO'
 
 try {
   Uninstall-ChocolateyPackage $package 'MSI' -SilentArgs "$msiId /qb" -validExitCodes @(0)
-  if ([IntPtr]::Size -eq 8) { Uninstall-ChocolateyPackage $package 'MSI' -SilentArgs "$msiId64 /qb" -validExitCodes @(0) }
+  if ([Environment]::IsIs64BitOperatingSystem) { Uninstall-ChocolateyPackage $package 'MSI' -SilentArgs "$msiId64 /qb" -validExitCodes @(0) }
   # the following is all part of error handling
   Write-ChocolateySuccess $package
 } catch {
