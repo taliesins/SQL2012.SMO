fe3d26414ca7bfe3308f9e86a3d3757e1bd36a4c
Taliesin Sisson <taliesins@yahoo.com>
2020-11-22 11:19:56 +0000

    Update to sp4

--- tools/chocolateyInstall.ps1
+++ tools/chocolateyInstall.ps1
@@ -2,11 +2,11 @@ $params = @{
   packageName = 'sql2012.smo';
   installerType = 'msi';
   silentArgs = '/quiet';
-  url = 'http://download.microsoft.com/download/3/A/6/3A632674-A016-4E31-A675-94BE390EA739/ENU/x86/SharedManagementObjects.msi';
-  checksum='9D9D1D0F4D498C3D4A35AFA51075C48734F5A84628F1679092378F760D339756';
+  url = 'https://download.microsoft.com/download/F/3/C/F3C64941-22A0-47E9-BC9B-1A19B4CA3E88/ENU/x86/SharedManagementObjects.msi';
+  checksum='EA278E7006B73A82B40BEE093C804D596C3FDA8E1B8687A666A4F189D44BE0BE';
   checksumType='Sha256';
-  url64 = 'http://download.microsoft.com/download/3/A/6/3A632674-A016-4E31-A675-94BE390EA739/ENU/x64/SharedManagementObjects.msi';
-  checksum64 = '7FF7E967C20F2B7402603D0A7D468246308135D93CD369EB49D968FAC8239CEB';
+  url64 = 'https://download.microsoft.com/download/F/3/C/F3C64941-22A0-47E9-BC9B-1A19B4CA3E88/ENU/x64/SharedManagementObjects.msi';
+  checksum64 = '18731A3D1A6625D97364BF5846184C80E8434AC31D5D801D7BB76212B8E416AD';
   checksumType64 = 'Sha256';
   validExitCodes = @(0,3010)
 }
